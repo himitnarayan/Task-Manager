4a3e77a7e8972c423b40a56b76ecd122c0657a0b
himitnarayan <himitnarayan31@gmail.com>
2026-05-03 14:40:51 +0530

    Fix MongoDB URI parsing bug for Railway deployments

--- app/db.py
+++ app/db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo.uri_parser import parse_uri
 import os
 
 mongo_client = None
@@ -6,10 +7,19 @@ db = None
 
 def init_db(app):
     global mongo_client, db
-    mongo_client = MongoClient(app.config['MONGO_URI'])
-    db_name = app.config['MONGO_URI'].split('/')[-1].split('?')[0]
+    uri = app.config['MONGO_URI']
+    mongo_client = MongoClient(uri)
+    
+    # Safely extract database name from URI
+    try:
+        parsed_uri = parse_uri(uri)
+        db_name = parsed_uri.get('database')
+    except:
+        db_name = None
+        
     if not db_name:
         db_name = 'project_manager'
+        
     db = mongo_client[db_name]
 
 def get_db():
